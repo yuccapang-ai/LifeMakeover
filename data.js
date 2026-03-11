53752a4c22036009d23855e27977f350b7555fc8
yuccapang-ai <yuccapang@gmail.com>
2026-03-11 17:43:03 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,83 @@
+// data.js 测试极简版
+const suits = [
+  { 
+    id: "001", 
+    name: "绒兔心语", 
+    event: "限时五星", 
+    period: "复刻2次", 
+    parts: [
+        { id: "hair",   name: "发型" },
+        { id: "dress",  name: "连衣裙" },
+        { id: "waitao",  name: "外套" },
+        { id: "shangyi",  name: "上衣" },
+        { id: "xiazhuang",  name: "下装" },
+        { id: "wazi",  name: "袜子" },
+        { id: "xiezi",  name: "鞋子" },
+        { id: "maozi",  name: "帽子" },
+        { id: "fashi",  name: "发饰" },
+        { id: "ershi",  name: "耳饰" },
+        { id: "jingshi",  name: "颈饰" },
+        { id: "shouchi",  name: "手持物" }
+        ] 
+    },
+    { 
+    id: "002", 
+    name: "酣竹滚滚", 
+    event: "限时五星", 
+    period: "复刻2次", 
+    parts: [
+        { id: "hair",   name: "发型" },
+        { id: "dress",  name: "连衣裙" },
+        { id: "waitao",  name: "外套" },
+        { id: "shangyi",  name: "上衣" },
+        { id: "xiazhuang",  name: "下装" },
+        { id: "wazi",  name: "袜子" },
+        { id: "xiezi",  name: "鞋子" },
+        { id: "maozi",  name: "帽子" },
+        { id: "fashi",  name: "发饰" },
+        { id: "ershi",  name: "耳饰" },
+        { id: "jingshi",  name: "颈饰" },
+        { id: "shouchi",  name: "手持物" }
+    ] 
+    },
+    { 
+    id: "003", 
+    name: "透明序曲", 
+    event: "限时五星", 
+    period: "未复刻", 
+    parts: [
+        { id: "hair",   name: "发型" },
+        { id: "dress",  name: "连衣裙" },
+        { id: "waitao",  name: "外套" },
+        { id: "shangyi",  name: "上衣" },
+        { id: "xiazhuang",  name: "下装" },
+        { id: "wazi",  name: "袜子" },
+        { id: "xiezi",  name: "鞋子" },
+        { id: "maozi",  name: "帽子" },
+        { id: "fashi",  name: "发饰" },
+        { id: "ershi",  name: "耳饰" },
+        { id: "jingshi",  name: "颈饰" },
+        { id: "shouchi",  name: "手持物" }
+    ] 
+    },
+    { 
+    id: "004", 
+    name: "封印解除", 
+    event: "限时五星", 
+    period: "未复刻", 
+    parts: [
+        { id: "hair",   name: "发型" },
+        { id: "dress",  name: "连衣裙" },
+        { id: "waitao",  name: "外套" },
+        { id: "shangyi",  name: "上衣" },
+        { id: "xiazhuang",  name: "下装" },
+        { id: "wazi",  name: "袜子" },
+        { id: "xiezi",  name: "鞋子" },
+        { id: "maozi",  name: "帽子" },
+        { id: "fashi",  name: "发饰" },
+        { id: "ershi",  name: "耳饰" },
+        { id: "jingshi",  name: "颈饰" },
+        { id: "shouchi",  name: "手持物" }
+    ] 
+    }
+];
